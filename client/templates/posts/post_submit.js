7080bac7827b00011cb06e1ef26b25fde35258de
balibou <benjamin.cherion@gmail.com>
2015-03-05 19:20:46 +0100

    Adding security

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,7 +7,14 @@ Template.postSubmit.events({
 			question: e.target.question.value
 		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+		// post._id = Posts.insert(post);
+		// Router.go('postPage', post);
+
+		Meteor.call('postInsert', post, function(error, result) {
+			if (error)
+				return alert(error.reason);
+			
+			Router.go('postPage', {_id: result._id});
+		});
 	}
 });
