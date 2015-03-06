8e1d3160d8fb23b50f3e7fe3a8aabd12eef23839
balibou <benjamin.cherion@gmail.com>
2015-03-06 17:55:54 +0100

    User can have a username

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -1,1 +1,12 @@
 accountsUIBootstrap3.setLanguage('fr');
+
+Accounts.ui.config({
+  requestPermissions: {},
+  extraSignupFields: [{
+      fieldName: 'username',
+      fieldLabel: 'Username',
+      inputType: 'text',
+      visible: true,
+      saveToProfile: true
+  }]
+});
