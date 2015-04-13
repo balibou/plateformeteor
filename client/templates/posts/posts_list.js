c1e83bfc3104e5c9df0b75abf4b1ee80e96caf2a
Ethan Escareño <Ethaan>
2015-04-13 14:00:04 -0500

    adding email verification and update to meteor 1.1.0.2

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,3 +3,19 @@ Template.postsList.helpers({
 		return Posts.find({}, {sort: {submitted: -1}});
 	}
 });
+
+ //onCreated
+
+ Template.postsList.onCreated(function(){
+    if (Accounts._verifyEmailToken) {
+    Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
+      if (err != null) {
+        if (err.message = 'Verify email link expired [403]') {
+          console.log('Sorry this verification link has expired.')
+        }
+      } else {
+        console.log('Thank you! Your email address has been confirmed.')
+      }
+    });
+  }
+})
